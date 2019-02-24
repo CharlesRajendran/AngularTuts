ac73322e5b549df31f6dde519135fe89ca32f6b9
charlesrajendran <charlesrajendran44@gmail.com>
2019-02-24 21:53:57 +0530

    Unit 1 - Data and Event Binding

--- application-test/src/app/app.component.ts
+++ application-test/src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  imageUrl = 'https://raw.githubusercontent.com/dart-lang/logos/master/logos_and_wordmarks/angulardart-logo.png';
+  showClass = true;
+  bgcolor = 'red';
+  marginSize = 100;
+
+  callFunction(e) {
+    console.log(e.target);
+  }
+
+  divCalled() {
+    console.log('A div has been called');
+  }
+
+  let twoWay:string;
 }
+
